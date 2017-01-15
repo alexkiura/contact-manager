19ef49d0932d289c4520afb2ec0ee5a3cfc70fde
Alex Kiura <kiuraalex@gmail.com>
2017-01-15 20:12:40 +0300

    feat: add contact app urls

---
+++ contacts/urls.py
@@ -0,0 +1,13 @@
+"""Url configuration for contacts."""
+from django.conf.urls import url, include
+from contacts.views import ContactViewset
+from rest_framework.routers import DefaultRouter
+
+
+router = DefaultRouter()
+router.register('contacts', ContactViewset)
+
+
+urlpatterns = [
+    url(r'^', include(router.urls))
+]
