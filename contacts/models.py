e953cd4ec9886042ab670987c3e309ba7e8683e1
Alex Kiura <kiuraalex@gmail.com>
2017-01-15 20:14:11 +0300

    chore: refactor models

--- contacts/models.py
+++ contacts/models.py
@@ -6,3 +6,6 @@ class Contact(models.Model):
     name = models.CharField(max_length=250, blank=False)
     telephone_number = models.CharField(max_length=16)
     email = models.EmailField(max_length=254, blank=True, default='')
+
+    def __str__(self):
+        return '{} {}'.format(self.name, self.telephone_number)
