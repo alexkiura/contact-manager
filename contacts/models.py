ce3a1e006d83bfacab914781f3379d77efa09b61
Alex Kiura <kiuraalex@gmail.com>
2017-01-24 06:05:42 +0300

    chore: add docstrings

--- contacts/models.py
+++ contacts/models.py
@@ -1,7 +1,10 @@
+"""Application models."""
+
 from django.db import models
 
 
 class Contact(models.Model):
+    """Contact model."""
     name = models.CharField(max_length=250, blank=False)
     telephone_number = models.CharField(max_length=16)
     email = models.EmailField(max_length=254, blank=True, default='')
