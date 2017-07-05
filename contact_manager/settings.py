bdf12e7f8678206c0b314db4c7d6ee74e63144a0
Alex Kiura <alex.kiura@andela.com>
2017-07-05 07:36:50 +0100

    chore: use postgres since it can handle many db connections

--- contact_manager/settings.py
+++ contact_manager/settings.py
@@ -52,10 +52,20 @@ TEMPLATES = [
 WSGI_APPLICATION = 'contact_manager.wsgi.application'
 
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'contacts',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+
     }
 }
 
