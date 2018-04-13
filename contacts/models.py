9aa84128111605417872666856e64ca8e435c320
Alex Kiura <kiuraalex@gmail.com>
2018-04-13 10:10:57 +0300

    feat: add person model

--- contacts/models.py
+++ contacts/models.py
@@ -8,6 +8,12 @@ class Contact(models.Model):
     name = models.CharField(max_length=250, blank=False)
     telephone_number = models.CharField(max_length=16)
     email = models.EmailField(max_length=254, blank=True, default='')
+    nick_name = models.CharField(max_length=15, blank=True)
 
     def __str__(self):
         return '{} {}'.format(self.name, self.telephone_number)
+
+class Person(models.Model):
+    """Person model"""
+    email = models.EmailField(max_length=254, blank=True, unique=True)
+    name = models.CharField(max_length=255, blank=True)
